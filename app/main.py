c35d1611336dc07239dedf6a0adba1e1f37f7b70
Aleksandr <kostromin63@mail.ru>
2024-11-04 19:26:33 +0300

    Исправление ошибок

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from routers import task, user
 
+from routers import task, user
 
 app = FastAPI()
 
@@ -8,7 +8,6 @@ app = FastAPI()
 @app.get('/')
 async def welcom() -> dict:
     return {"message": "Welcome to Taskmanager"}
-
 
 app.include_router(user.router)
 app.include_router(task.router)
--- app/schemas.py
+++ app/schemas.py
@@ -2,25 +2,25 @@ from pydantic import BaseModel
 
 
 class CreateUser(BaseModel):
-    username = str
-    firstname = str
-    lastname = str
-    age = int
+    username: str
+    firstname: str
+    lastname: str
+    age: int
 
 
 class UpdateUser(BaseModel):
-    firstname = str
-    lastname = str
+    firstname: str
+    lastname: str
     age = int
 
 
 class CreateTsdk(BaseModel):
-    title = str
-    content = str
-    prioritY = int
+    title: str
+    content: str
+    prioritY: int
 
 
 class UpdateTsdk(BaseModel):
-    title = str
-    content = str
-    prioritY = int
+    title: str
+    content: str
+    prioritY: int
